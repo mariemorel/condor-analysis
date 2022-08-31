a1308a694282e2c3b45b368c2c2f64f4ace710c7
Marie  MOREL <mamorel@maestro-submit.maestro.pasteur.fr>
2022-08-31 15:06:31 +0200

    fade script and files

--- src/fade_command.sh
+++ src/fade_command.sh
@@ -1,17 +1,52 @@
+#FADE version 0.2
 
-#### rhodospine
-##conjunction
-/pasteur/zeus/projets/p01/Evolbioinfo/users/mamorel/Projet_Convergence/Data/args/processed/rhodopsin/lessgappy.align_reroot.fa
-/pasteur/zeus/projets/p01/Evolbioinfo/users/mamorel/Projet_Convergence/Data/args/processed/rhodopsin/FADE/tree_rerooted_foreground_conjunction.nhx
-sbatch --qos bioevo -p bioevo --wrap="./hyphy fade --alignment rhodopsin/lessgappy.align_reroot.fa --tree rhodopsin/FADE/tree_rerooted_foreground_conjunction.nhx --output rhodopsin/FADE/foreground_aa.LG.FADE.json --branches Foreground --model LG --cache rhodopsin/FADE/foreground_aa.mtZOA.FADE.cache"
+### Sedges c3c4
+sbatch --qos bioevo -p bioevo --mem-per-cpu 10G --wrap="./hyphy fade \\
+--alignment data/c3c4/cyp_coding.aa.coor_mays.fa \\
+--tree data/c3c4/cyp_coding.phy_phyml_tree_foreground.txt \\
+--output results/c3c4/FADE/foreground_aaJTT.FADE.json \\
+--cache results/c3c4/FADE/foreground_aaJTT.FADE.cache \\
+--branches Foreground --model JTT"
 
-
-
-### c3c4
-sbatch --qos bioevo -p bioevo --mem-per-cpu 10G --wrap="./hyphy fade --alignment C3C4/cyp_coding.aa.coor_mays.fa --tree C3C4/FADE/cyp_coding.phy_phyml_tree_foreground.txt --output C3C4/FADE/foreground_aa.FADE.json --branches Foreground"
-sbatch --qos bioevo -p bioevo --mem-per-cpu 10G --wrap="./hyphy fade --alignment c3c4/new_homologues/align.subseq.alignment.aa.fasta --tree c3c4/new_homologues/align.subseq.alignment.aa.foregroundc3andc4.treefile --output c3c4/new_homologues/foreground_aa_c3c4.FADE.json --cache c3c4/new_homologues/foreground_aa_c3c4.FADE.cache --branches Foreground --model JTT"
-sbatch --qos bioevo -p bioevo --mem-per-cpu 10G --wrap="./hyphy fade --alignment c3c4/no_c3c4_clade/cyp_coding_noc3c4.aa.coor_mays.fa --tree c3c4/no_c3c4_clade/foreground_aa_c4_pheno.FADE.cache --branches Foreground --model JTT"
-
+sbatch --qos bioevo -p bioevo --mem-per-cpu 10G --wrap="./hyphy fade \\
+--alignment data/c3c4/pheno_noc3c4/cyp_coding_noc3c4.aa.coor_mays.fa \\
+--tree data/c3c4/pheno_noc3c4/cyp_coding_noc3c4_foreground.phy_phyml_tree.txt
+--cache results/c3c4/FADE/foreground_aa_noc3c4_pheno.FADE.cache \\
+--output results/c3c4/FADE/foreground_aa_noc3c4_pheno.FADE.json \\
+--branches Foreground --model JTT"
 
 ### HIV africa
-sbatch --qos bioevo -p bioevo --mem-per-cpu 10G --wrap="./hyphy fade --model HIVBm --alignment africa/recomb_jphmm/align.noCRF.jphmm_outgroup.aa.fa --tree africa/recomb_jphmm/foreground_tips_conjunction_aa.tree --output africa/recomb_jphmm/foreground_aa_conjunction.FADE.json --branches Foreground"
+sbatch --qos bioevo -p bioevo --mem-per-cpu 10G --wrap="./hyphy fade \\
+--model HIVBm \\
+--alignment data/HIV_africa/align.noCRF.jphmm_outgroup.aa.fa \\
+--tree data/HIV_africa/foreground_tips_conjunction_aa.tree \\
+--output results/HIV_africa/FADE/foreground_aa_conjunction_HIVb.FADE.json \\
+--branches Foreground"
+
+#model misspecification JTT
+sbatch --qos bioevo -p bioevo --mem-per-cpu 10G --wrap="./hyphy fade \\
+--model JTT \\
+--alignment data/HIV_africa/align.noCRF.jphmm_outgroup.aa.fa \\
+--tree data/HIV_africa/foreground_tips_conjunction_aa.tree \\
+--output results/HIV_africa/FADE/foreground_aa_conjunction_JTT.FADE.json \\
+--branches Foreground"
+
+
+#### rhodospine
+##fresh/brackish
+sbatch --qos bioevo -p bioevo --wrap="./hyphy fade \\
+--alignment data/rhodopsin/lessgappy.align_reroot.fa \\
+--tree data/rhodopsin/tree_rerooted_foreground_conjunction.nhx \\
+--output results/rhodopsin/FADE/foreground_aa_conjunction_brackish.LG.FADE.json \\
+--branches Foreground \\
+--model LG \\
+--cache results/rhodopsin/FADE/foreground_aa_conjunction_brackish.LG.FADE.cache"
+
+###marine
+sbatch --qos bioevo -p bioevo --wrap="./hyphy fade \\
+--alignment data/rhodopsin/lessgappy.align_reroot.fa \\
+--tree data/rhodopsin/tree_rerooted_foreground_marine_conjunction.nhx \\
+--output results/rhodopsin/FADE/foreground_aa_conjunction_marine.LG.FADE.json \\
+--branches Foreground \\
+--model LG \\
+--cache results/rhodopsin/FADE/foreground_aa_conjunction_marine.LG.FADE.cache"
